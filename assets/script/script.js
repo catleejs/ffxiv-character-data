2e4b105f429511ec371c7cbab67621ee4f72921d
Killian Chamberlain <killian7195@gmail.com>
2021-09-16 20:55:40 -0500

    api edits

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,10 +1,21 @@
 const ffxivKey = "a308a0bb8cc54a5c8f2f0c8bcb094b6641792b1ef5454012abfc2abb1dad94e3";
 const ffxivUrl = "https://xivapi.com/";
 const characterSearch = "character/search";
+const characterid = "character/";
 
 let lastRes = '';
 const fetchCharacterSearch = function(charname) {
   return fetch(ffxivUrl + characterSearch + '?name=' + charname + '&private_key=' + ffxivKey)
+  .then(function (res){
+    return res.json();
+  }).then(function(json) {
+    console.log(json);
+    lastRes = json;
+  });
+}
+
+const fetchcharacterid = function(charid) {
+  fetch(ffxivUrl + characterid + charid + "?private_key=" + ffxivKey)
   .then(function (res){
     return res.json();
   }).then(function(json) {
