26af1712fdb27476310d30aa35ffa0647053df14
Lance Kelley <lancekkcodes@gmail.com>
2021-09-16 19:59:16 -0500

    initial xivapi exploration

---
+++ assets/script/script.js
@@ -0,0 +1,14 @@
+const ffxivKey = "a308a0bb8cc54a5c8f2f0c8bcb094b6641792b1ef5454012abfc2abb1dad94e3";
+const ffxivUrl = "https://xivapi.com/";
+const characterSearch = "character/search";
+
+let lastRes = '';
+const fetchCharacterSearch = function(charname) {
+  return fetch(ffxivUrl + characterSearch + '?name=' + charname + '&private_key=' + ffxivKey)
+  .then(function (res){
+    return res.json();
+  }).then(function(json) {
+    console.log(json);
+    lastRes = json;
+  });
+}
